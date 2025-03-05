766edf9e1eb2baecc93e090b7eebb2578c4004c7
Austin Arbaugh <austinarbaugh@Austins-MacBook-Pro.local>
2025-03-05 14:05:56 -0500

    overhauled phrase editor and added template manager

--- phrases.js
+++ phrases.js
@@ -1,4 +1,4 @@
-const predefinedPhrases = {
+let predefinedPhrases = {
   "dispatch info": {
     "fields": [
       {
---
+++ templates.js
@@ -0,0 +1,12 @@
+let narrativeTemplates = {
+  "Refusal": {
+    "content": "Refusal",
+    "order": 1741201271987,
+    "name": "Refusal"
+  },
+  "Regular": {
+    "content": "D- {dispatch info}.\n\nC- {complaints}\n\nH- \n\nA- {arrival}\n\nABC: {abc}\nNEURO: {neuro}\nHEENT: {heent}\nCV: {cv}\nPULM: {pulm}\nGI/GU/ABD: {abd/gi/gu}\nMS/Ortho/Skin: {ms/ortho/skin}\nSOC/PSYCH: {soc/psych}\n\nDuring transport Pt remained stable.\n\nR- (Scene) Vitals and an assessment was performed. (Transport) Pt was monitored and vitals were reassessed as needed.\n\nT- Once Pt gave verbal consent to be transported to the hospital the {move to stretcher}. Once the Pt was secured the stretcher was moved out to the ambulance without incident. The stretcher was loaded into the ambulance and secured via an auto-load system. {transport}. Upon arrival to the hospital the stretcher was unloaded and the Pt was taken inside without incident. Once inside, the Pt was registered and taken to {hospital transfer}. At this time a verbal report was given to a nurse and care was transferred.\n\nE- None",
+    "order": 1000,
+    "name": "Regular"
+  }
+}
